ae06c011ec66cf424fafd2d4924ad77d8498b9da
Saurav Ambuskar <167842485+Sauravambuskar@users.noreply.github.com>
2026-09-12 02:11:16 +0530

    Update S3 bucket name for Terraform backend

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -6,7 +6,7 @@
 # inert until you configure and init with a real backend.
 terraform {
   backend "s3" {
-    bucket       = "banking-platform-tfstate-118178010323" # create once, then set here
+    bucket       = "banking-platform-tfstate-118178010323-847291" # create once, then set here
     key          = "dev/networking.tfstate"
     region       = "us-east-1"
     use_lockfile = true
